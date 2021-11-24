a495851a15acb82eefccc0d18e4610057f75e8aa
Gourav Kadam <gouravkadam49@gmail.com>
2021-11-24 21:31:36 +0530

    guess number

--- script.js
+++ script.js
@@ -10,6 +10,11 @@
 // document.querySelector('.guess').value = 23 ;
 // console.log(document.querySelector('.guess').value);
 
+const secretnumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretnumber;
+
+let score = 20 ;
+
 document.querySelector('.check').addEventListener
 ('click' , function(){
   const guess = Number(document.querySelector('.guess').value);
@@ -17,5 +22,24 @@ document.querySelector('.check').addEventListener
 
   if(!guess){
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess == secretnumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess < secretnumber) {
+    if(score > 0){
+      document.querySelector('.message').textContent = 'number is too Low';
+      score--;
+      document.querySelector('.score').textContent = score ;
+    }else{
+      document.querySelector('.message').textContent = 'You lose the game';
+    }
+  } else if (guess > secretnumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'number is too high';
+      score--;
+      document.querySelector('.score').textContent = score ;
+    }else{
+      document.querySelector('.message').textContent = 'You lose the game';
+    }
+
   }
 });
