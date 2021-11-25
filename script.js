454330d99731219d684676ed8e128df4f45537de
Gourav Kadam <gouravkadam49@gmail.com>
2021-11-25 21:29:42 +0530

    Implementing Highscore

--- script.js
+++ script.js
@@ -10,23 +10,25 @@
 // document.querySelector('.guess').value = 23 ;
 // console.log(document.querySelector('.guess').value);
 
-const secretnumber = Math.trunc(Math.random()*20)+1;
-document.querySelector('.number').textContent = secretnumber;
+let secretnumber = Math.trunc(Math.random()*20)+1;
+
+console.log(secretnumber);
 
 let score = 20 ;
 
 document.querySelector('.check').addEventListener
 ('click' , function(){
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
+  //console.log(guess);
 
   if(!guess){
     document.querySelector('.message').textContent = 'No Number';
   } else if (guess == secretnumber) {
     document.querySelector('.message').textContent = 'Correct Number';
-
+    document.querySelector('.number').textContent = secretnumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = score;
 
   } else if (guess < secretnumber) {
     if(score > 0){
@@ -46,4 +48,17 @@ document.querySelector('.check').addEventListener
     }
 
   }
+});
+
+document.querySelector('.again').addEventListener("click" , function(){
+    score = 20;
+    secretnumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    console.log(secretnumber);
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
+    //location.reload()
 });
